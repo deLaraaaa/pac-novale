eaffe837240645b64f09f4ec4a1f886a8b8fff40
Rafael de Lara <rafaeldrozdek@gmail.com>
2024-11-29 20:47:19 -0300

    chore: remove logs

--- server/firebase-config.js
+++ server/firebase-config.js
@@ -2,7 +2,6 @@ import admin from "firebase-admin";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,7 +11,6 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 const serviceAccountKey = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
 
 const serviceAccount = JSON.parse(serviceAccountKey);
-console.info("firebase-config.js | FTH.RL.11 | ", serviceAccountKey);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
