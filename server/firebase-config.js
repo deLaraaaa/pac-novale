9f6cb9455a84174e70b4eb174617c3f79965b782
Rafael de Lara <rafaeldrozdek@gmail.com>
2024-11-29 20:44:59 -0300

    fix: working dotenv

--- server/firebase-config.js
+++ server/firebase-config.js
@@ -1,9 +1,18 @@
 import admin from "firebase-admin";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+const serviceAccountKey = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+
+const serviceAccount = JSON.parse(serviceAccountKey);
+console.info("firebase-config.js | FTH.RL.11 | ", serviceAccountKey);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
